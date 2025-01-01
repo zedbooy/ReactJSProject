950b3e2f4f73ed40a10fa7d190975a671ff0153f
Ziad Ghalbane <98329069+zedbooy@users.noreply.github.com>
2025-01-01 17:43:00 +0100

    adding get started forum that was removed
    push only what you changed

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -5,15 +5,12 @@ const GetStarted = () => {
     <div id="get-started" className="g-wrapper">
       <div className="paddings innerWidth g-container">
         <div className="flexColCenter inner-container">
-          <span className="primaryText">Get started with Marrakech Riads</span>
+          <span className="primaryText">Get started with Homyz</span>
           <span className="secondaryText">
             Subscribe and find super attractive price quotes from us.
             <br />
-            Find your riad soon
+            Find your residence soon
           </span>
-          <button className="button" href>
-            <a href="./test.html">Get Started</a>
-          </button>
         </div>
       </div>
     </div>
